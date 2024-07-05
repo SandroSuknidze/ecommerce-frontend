38c2d9959f1e184a9590051b8b4281b743208218
SandroLineate <sandro@gmail.com>
2024-07-05 14:09:49 +0400

    Search responsiveness

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,6 +4,7 @@ import SearchIcon from '@/public/assets/SearchIcon'
 import ProductCard from '@/components/ProductCard'
 import collection2 from '@/public/assets/collections/collection2.webp'
 import { useLockBodyScroll } from 'react-use'
+import { useEffect, useState } from 'react'
 
 interface SearchProps {
     toggleSearch: () => void,
@@ -12,12 +13,36 @@ interface SearchProps {
 function Search({ toggleSearch }: SearchProps) {
     useLockBodyScroll(true)
 
+
+    const [numCols, setNumCols] = useState(0); // Default to 3 columns
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 1200 && window.innerWidth >= 992) {
+                setNumCols(5);
+            } else if (window.innerWidth <= 992 && window.innerWidth >= 768) {
+                setNumCols(4);
+            } else if (window.innerWidth <= 768 && window.innerWidth >= 576) {
+                setNumCols(3);
+            } else if (window.innerWidth <= 576 && window.innerWidth >= 320) {
+                setNumCols(2);
+            } else {
+                setNumCols(6);
+            }
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className="fixed inset-0 !z-[70] flex items-center justify-center">
             <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="absolute top-0 !z-50 h-[90vh] w-full overflow-y-scroll bg-white px-[30px] py-[60px]">
+            <div className="absolute top-0 !z-50 h-[90vh] w-full overflow-y-scroll bg-white px-[30px] py-[60px] xl:px-[15px]">
                 <div className="m-auto max-w-[1410px]">
-                    <h4 className="mb-[20px] text-center text-[32px] font-medium text-[#111111]">
+                    <h4 className="mb-[20px] text-center text-[32px] font-medium text-[#111111] md:text-[24px]">
                         Search Our Site
                     </h4>
                     <div className="flex cursor-pointer justify-end">
@@ -27,11 +52,11 @@ function Search({ toggleSearch }: SearchProps) {
                             onClick={() => toggleSearch()}
                         />
                     </div>
-                    <form className="relative m-auto w-[66%] text-center">
+                    <form className="relative m-auto w-[66%] text-center md:w-full">
                         <input
                             type="text"
                             placeholder="I'm looking for..."
-                            className="mx-auto mr-[10px] w-full rounded-[30px] border-[1px] border-[#ebebeb] py-[10px] pl-[20px] pr-[50px] text-[14px] leading-[28px] outline-0 transition duration-300 placeholder:text-[#555555] focus:border-[1px] focus:border-[#131313] focus:transition focus:duration-300"
+                            className="mx-auto w-full rounded-[30px] border-[1px] border-[#ebebeb] py-[10px] pl-[20px] pr-[50px] text-[14px] leading-[28px] outline-0 transition duration-300 placeholder:text-[#555555] focus:border-[1px] focus:border-[#131313] focus:transition focus:duration-300"
                         />
                         <div className="absolute right-[20px] top-[16px] cursor-pointer">
                             <SearchIcon />
@@ -54,7 +79,9 @@ function Search({ toggleSearch }: SearchProps) {
                             Popular Products
                         </h4>
                     </div>
-                    <div className="mt-[20px] grid w-full grid-cols-6 gap-[20px] xl:grid-cols-4 h-max">
+                    <div className="mt-[20px] grid w-full grid-cols-6 gap-[20px] h-max"
+                         style={{ gridTemplateColumns: `repeat(${numCols}, minmax(0, 1fr))` }}
+                        >
                         <ProductCard
                             title={'Square Textured Striped'}
                             imageSrc={collection2}
