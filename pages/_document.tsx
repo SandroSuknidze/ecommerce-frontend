53c75810f34310f0f3006e7963a35352a93b754d
SandroLineate <sandro@gmail.com>
2024-07-19 21:53:44 +0400

    Language bug attempt fix #9

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+// @ts-ignore
+import i18nextConfig from '../next-i18next.config';
 
 class MyDocument extends Document {
     render() {
+        const currentLocale =
+            this.props.__NEXT_DATA__.locale || i18nextConfig.i18n.defaultLocale;
         return (
-            <Html lang="en">
+            <Html lang={currentLocale}>
                 <Head>
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css?v=7857324" />
                     <title>Umino</title>
                 </Head>
                 <body>
-                <Main />
-                <NextScript />
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         );
